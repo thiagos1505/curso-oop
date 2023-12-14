e920018033b42a285c8ce134412d2e50a972b9f0
thiagos1505 <thiagosingulano@hotmail.com>
2023-12-13 21:16:20 -0300

    adiciona o conteudo da aula sobre construtores

---
+++ modulo5/Produto.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace modulo5
+{
+    public class Produto
+    {
+        public string? Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade) {
+           
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        
+        
+        public double ValorTotalEmEstoque() {
+        return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade) {
+        Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {
+        Quantidade -= quantidade;
+        }
+        public override string ToString() {
+        return Nome
+        + ", $ "
+        + Preco.ToString("F2", CultureInfo.InvariantCulture)
+        + ", "
+        + Quantidade
+        + " unidades, Total: $ "
+        + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
